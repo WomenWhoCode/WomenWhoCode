44e1e705322360250157134bd3fa4a29c566696c
Shehba Shahab <shehbashahab@gmail.com>
2016-03-15 21:15:45 -0700

    Remove Toasts that are commented out and reformat project according to AndroidStyle.xml

--- app/src/main/java/com/womenwhocode/womenwhocode/activities/UserProfileActivity.java
+++ app/src/main/java/com/womenwhocode/womenwhocode/activities/UserProfileActivity.java
@@ -112,9 +112,6 @@ public class UserProfileActivity extends AppCompatActivity {
                         networks.add(networkList.get(i).getString("title"));
                         Collections.sort(networks);
                     }
-
-                    //  Toast.makeText(getApplicationContext(),networkList.get(0).getString("title"),Toast.LENGTH_LONG).show();
-
                 } else {
                     Log.d("score", "Error: " + e.getMessage());
                 }
@@ -343,7 +340,6 @@ public class UserProfileActivity extends AppCompatActivity {
                     // Load the taken image into a preview
                     ivPic.setImageBitmap(finalImg);
                 } else { // Result was a failure
-//                    Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
                 }
             }
 
@@ -357,8 +353,6 @@ public class UserProfileActivity extends AppCompatActivity {
                 userProfile.setPhotoFile(imgFile);
             }
         } catch (Exception e) {
-//            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
-//                    .show();
         }
     }
 
